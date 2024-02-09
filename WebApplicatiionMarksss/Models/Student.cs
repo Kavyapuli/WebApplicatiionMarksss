d3e909a2ec880ab3b020ecf85db5bb72f1ab9286
Kavyapuli <pulikavya10@gmail.com>
2024-02-09 19:06:22 +0530

    Add project files.

---
+++ WebApplicatiionMarksss/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplicatiionMarksss.Models
+{
+    public partial class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; } = null!;
+        public int? ClassId { get; set; }
+        public int? SubjectId { get; set; }
+        public string? Marks { get; set; }
+
+        public virtual Class? Class { get; set; }
+        public virtual Subject? Subject { get; set; }
+    }
+}
